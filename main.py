91898d50e0dc62013c86a88d433d8e665405177a
Ademola Oladapo <ademolaoladapo@gmail.com>
2024-02-28 18:57:50 +0100

    code repurposed

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 text = 'Hello World'
 shift = 3
-encrypted_text =''
+
 
 def caesar():
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
 
     for char in text.lower():
+        
         # Ensure not text character are taken care of by just appending them
-        if char.isalpha():
+        if not char.isalpha():
             encrypted_text += char
         else:
             # Find the index of char in the string alphabet
-            index = alphabet.index(char)
+            index = alphabet.find(char)
             new_index = (index + shift) % len(alphabet)
-            new_char = alphabet[new_index]
-            encrypted_text.append(new_char)
+            encrypted_text += alphabet[new_index]
     print(encrypted_text)
 
+caesar()
